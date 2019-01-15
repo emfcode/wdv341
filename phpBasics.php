5c5879ec224faf4beafa52a64e3b2861fa07be5c
emfcode <46467359+emfcode@users.noreply.github.com>
2019-01-14 19:37:45 -0600

    added the last step of assignment that I missed

--- phpBasics.php
+++ phpBasics.php
@@ -9,6 +9,19 @@
         var number1 = 5;
         var number2 = 8;
         var total = number1 + number2;
+
+        var lang = <?php echo  "['PHP','HTML','JavaScript']"?>
+
+        <?php 
+            $php = "PHP";
+            $html = "HTML";
+            $javascript = "JavaScript";
+        ?>
+
+        var lang2 = <?php echo "['$php','$html','$javascript']"; ?>
+
+        var lang3 = <?php echo "['".$php."','".$html."','".$javascript."']"; ?>
+
     </script>
 
     <?php $yourName = "Eric Fenimore"; ?>
@@ -25,13 +38,15 @@
         ?>
     </h2>
 
-    <p><?php
-        echo "<script type=\"text/javascript\">
+    <p><?php echo 
+            "<script type=\"text/javascript\">
                 document.write('<p>'+number1+'</p>');
                 document.write('<p>'+number2+'</p>');
                 document.write('<p>'+total+'</p>');
             </script>"
     ?></p>
+    
+    <p id="arrayOut"><script>document.write(lang); document.write(lang2); document.write(lang3);</script></p>
 
 </body>
 </html>
